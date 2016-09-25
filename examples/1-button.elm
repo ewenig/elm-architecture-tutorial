94ab335ff3c4df61256c0090f5c1a5c1091af354
Eli Wenig <eli@csh.rit.edu>
2016-09-24 21:51:57 -0700

    added Reset button to 1-button.elm

--- examples/1-button.elm
+++ examples/1-button.elm
@@ -31,6 +31,7 @@ model =
 type Msg
   = Increment
   | Decrement
+  | Reset
 
 
 update : Msg -> Model -> Model
@@ -42,6 +43,8 @@ update msg model =
     Decrement ->
       model - 1
 
+    Reset ->
+      0
 
 
 -- VIEW
@@ -53,4 +56,6 @@ view model =
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , div [] []
+    , button [ onClick Reset ] [ text "Reset " ]
     ]
